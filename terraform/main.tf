2c3a0eec224fbfb90d21d950afe815194d8a6e4f
kwehen <kwesihenry11@gmail.com>
2024-10-26 16:28:26 -0400

    added talos nodes via terraform

---
+++ terraform/main.tf
@@ -0,0 +1,101 @@
+resource "proxmox_vm_qemu" "control_plane_nodes" {
+  for_each = var.control_plane_nodes
+
+  name        = each.value.name
+  desc        = "Talos Control Plane Node"
+  vmid        = each.value.vmid
+  target_node = each.value.target_node
+
+  onboot   = false
+  memory   = 8192
+  sockets  = 1
+  cores    = 2
+  cpu      = "host"
+  vm_state = "stopped"
+
+  network {
+    model    = "virtio"
+    macaddr  = each.value.macaddr
+    bridge   = "vmbr0"
+    firewall = false
+    tag      = 4
+  }
+
+  disks {
+    ide {
+      ide2 {
+        cdrom {
+          iso = "prox_iso:iso/metal-amd64.iso"
+        }
+      }
+    }
+    scsi {
+      scsi0 {
+        disk {
+          storage = "local-lvm"
+          size    = "64G"
+          cache   = "writethrough"
+        }
+      }
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      network,
+      disk,
+      vm_state
+    ]
+  }
+}
+
+resource "proxmox_vm_qemu" "longhorn_worker_nodes" {
+  for_each = var.worker_nodes
+
+  name        = each.value.name
+  desc        = "Talos Worker Node"
+  vmid        = each.value.vmid
+  target_node = each.value.target_node
+
+  onboot   = false
+  memory   = each.value.memory
+  sockets  = 1
+  cores    = each.value.cores
+  cpu      = "host"
+  vm_state = "stopped"
+
+  network {
+    model    = "virtio"
+    macaddr  = each.value.macaddr
+    bridge   = "vmbr0"
+    firewall = false
+    tag      = 4
+  }
+
+  disks {
+    ide {
+      ide2 {
+        cdrom {
+          iso = "prox_iso:iso/metal-amd64.iso"
+        }
+      }
+    }
+    scsi {
+      scsi0 {
+        disk {
+          storage = each.value.storage
+          size    = "800G"
+          cache   = "writethrough"
+        }
+      }
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      network,
+      disk,
+      vm_state
+    ]
+  }
+}
