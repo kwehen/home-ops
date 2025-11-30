6bdea5a0af5dcbee1eaff36ac2ffc8c15621a2a4
kwehen <kwesihenry11@gmail.com>
2025-11-30 15:54:33 -0500

    feat: hello I am back

--- terraform/main.tf
+++ terraform/main.tf
@@ -51,55 +51,55 @@ resource "proxmox_vm_qemu" "control_plane_nodes" {
   }
 }
 
-resource "proxmox_vm_qemu" "longhorn_worker_nodes" {
-  for_each = var.worker_nodes
+# resource "proxmox_vm_qemu" "longhorn_worker_nodes" {
+#   for_each = var.worker_nodes
 
-  name        = each.value.name
-  desc        = "Talos Worker Node"
-  vmid        = each.value.vmid
-  target_node = each.value.target_node
+#   name        = each.value.name
+#   desc        = "Talos Worker Node"
+#   vmid        = each.value.vmid
+#   target_node = each.value.target_node
 
-  onboot   = false
-  memory   = each.value.memory
-  sockets  = 1
-  cores    = each.value.cores
-  cpu_type = "host"
-  vm_state = "stopped"
-  scsihw   = "virtio-scsi-single"
+#   onboot   = false
+#   memory   = each.value.memory
+#   sockets  = 1
+#   cores    = each.value.cores
+#   cpu_type = "host"
+#   vm_state = "stopped"
+#   scsihw   = "virtio-scsi-single"
 
-  network {
-    id       = 0
-    model    = "virtio"
-    macaddr  = each.value.macaddr
-    bridge   = "vmbr0"
-    firewall = false
-    tag      = 4
-  }
+#   network {
+#     id       = 0
+#     model    = "virtio"
+#     macaddr  = each.value.macaddr
+#     bridge   = "vmbr0"
+#     firewall = false
+#     tag      = 4
+#   }
 
-  disks {
-    ide {
-      ide2 {
-        cdrom {
-          iso = "prox_iso:iso/metal-amd64.iso"
-        }
-      }
-    }
-    scsi {
-      scsi0 {
-        disk {
-          storage = each.value.storage
-          size    = "625G"
-          cache   = "writethrough"
-        }
-      }
-    }
-  }
+#   disks {
+#     ide {
+#       ide2 {
+#         cdrom {
+#           iso = "prox_iso:iso/metal-amd64.iso"
+#         }
+#       }
+#     }
+#     scsi {
+#       scsi0 {
+#         disk {
+#           storage = each.value.storage
+#           size    = "625G"
+#           cache   = "writethrough"
+#         }
+#       }
+#     }
+#   }
 
-  lifecycle {
-    ignore_changes = [
-      network,
-      disk,
-      vm_state
-    ]
-  }
-}
+#   lifecycle {
+#     ignore_changes = [
+#       network,
+#       disk,
+#       vm_state
+#     ]
+#   }
+# }
